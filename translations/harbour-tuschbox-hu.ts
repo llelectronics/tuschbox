e836a1f2bf505be2f0ca15cc7edb257ebeb75220
Miklos Marton <martonmiklosqdev@gmail.com>
2016-12-18 22:34:26 +0100

    Added Hungarian translation. Added QML files to the pro file in the lupdate_only section to be able to update them easier with lupdate. Added qsTr to some of the untranslated strings.

---
+++ translations/harbour-tuschbox-hu.ts
@@ -0,0 +1,144 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hu_HU">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>A simple soundboard application.</source>
+        <translation>Egy letisztult zenedoboz alkalmazás.</translation>
+    </message>
+    <message>
+        <source>License: GPL (Version 3)</source>
+        <translation>Licensz: GPL (V3)</translation>
+    </message>
+    <message>
+        <source>Created by llelectronics</source>
+        <translation>Készítette: llelectronics</translation>
+    </message>
+    <message>
+        <source>&lt;a href=&quot;https://github.com/llelectronics/tuschbox&quot;&gt;Sourcecode on Github&lt;/a&gt;</source>
+        <translation>&lt;a href=&quot;https://github.com/llelectronics/tuschbox&quot;&gt;Forráskód a Github-on&lt;/a&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>My Cover</source>
+        <translation>Borítóm</translation>
+    </message>
+</context>
+<context>
+    <name>EditButton</name>
+    <message>
+        <source>Edit Button</source>
+        <translation>Szerkesztés gomb</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>Cím</translation>
+    </message>
+    <message>
+        <source>Soundfile</source>
+        <translation>Hangfájl</translation>
+    </message>
+    <message>
+        <source>Choose Soundfile</source>
+        <translation>Hangfájl kiválasztása</translation>
+    </message>
+    <message>
+        <source>Play</source>
+        <translation>Lejátszás</translation>
+    </message>
+    <message>
+        <source>Select Soundfile</source>
+        <translation>Hangfájl kiválasztása</translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation>Leállítás</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Mentés</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Törlés</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>About </source>
+        <translation>Az alkalmazásról  </translation>
+    </message>
+    <message>
+        <source>New Soundboard</source>
+        <translation>Új panel</translation>
+    </message>
+    <message>
+        <source>Save Soundboard</source>
+        <translation>Panel mentése</translation>
+    </message>
+    <message>
+        <source>Show Soundboards</source>
+        <translation>Panel kiválasztása</translation>
+    </message>
+    <message>
+        <source>Add Soundbbutton</source>
+        <translation>Gomb hozzáadása</translation>
+    </message>
+</context>
+<context>
+    <name>IconList</name>
+    <message>
+        <source>Icons</source>
+        <translation>Ikonok</translation>
+    </message>
+</context>
+<context>
+    <name>OpenDialog</name>
+    <message>
+        <source>Deleting </source>
+        <translation>Törlés  </translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Törlés</translation>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <source>Create</source>
+        <translation>Új</translation>
+    </message>
+    <message>
+        <source>Create new Soundboard</source>
+        <translation>Új zenedoboz panel</translation>
+    </message>
+    <message>
+        <source>Name of the new soundboard</source>
+        <translation>Új panel neve</translation>
+    </message>
+    <message>
+        <source>Soundboard Name</source>
+        <translation>Panel neve</translation>
+    </message>
+</context>
+<context>
+    <name>SoundBoardList</name>
+    <message>
+        <source>Soundboard List</source>
+        <translation>Panelek</translation>
+    </message>
+    <message>
+        <source>Deleting </source>
+        <translation>Törlés </translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Törlés</translation>
+    </message>
+</context>
+</TS>
